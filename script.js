58e71019b835d60876f6dac2987c09e27c018897
UniBreakfast <unibreakfast1@gmail.com>
2022-01-07 21:11:08 +0200

    restyle a bit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-fillWithSizeCells(wrapper, 100)
+fillWithSizeCells(wrapper, 50)
 
-onresize = () => fillWithSizeCells(wrapper, 100)
+onresize = () => fillWithSizeCells(wrapper, 50)
 
 function fillWithSizeCells(containerEl, size) {
   const rowCount = Math.ceil(innerHeight / size)
   const columnCount = Math.ceil(innerWidth / size)
-  const containerStyle = { width: columnCount * size + 'px', height: rowCount * size + 'px' }
   const divStyle = { width: size + 'px', height: size + 'px' }
+  const containerStyle = { width: columnCount * size + 'px', height: rowCount * size + 'px' }
 
   containerEl.replaceChildren()
   Object.assign(containerEl.style, containerStyle)
@@ -14,16 +14,15 @@ function fillWithSizeCells(containerEl, size) {
   for (let i = 0; i < rowCount; i++) {
     for (let j = 0; j < columnCount; j++) {
       const div = document.createElement('div')
-      const span = document.createElement('span')
       if (i % 2 && j % 2 || i % 2 == 0 && j % 2 == 0) {
-        Object.assign(div.style, {backgroundColor: 'grey', color: 'white'})
-        span.innerText = 'y: ' + (i + 1) * size + 'px'
+        div.innerText = 'y: ' + (i + 1) * size + 'px'
+        div.classList.add('y')
       } else {
-        span.innerText = 'x: ' + (j + 1) * size + 'px'
+        div.innerText = 'x:\n' + (j + 1) * size + '\npx'
+        div.classList.add('x')
       }
       Object.assign(div.style, divStyle)
       containerEl.append(div)
-      div.append(span)
     }
   }
 }
